c5f8046200a57082990054b077ee3e48b2c3df4e
LucasPagli <lucas.dsilva@sptech.school>
2024-04-20 17:56:25 -0300

    feat(hardware-api): Implementando monitoramento de Cpu

--- hardware-api/src/main/java/model/componente/cpu/MonitoramentoCpu.java
+++ hardware-api/src/main/java/model/componente/cpu/MonitoramentoCpu.java
@@ -5,9 +5,28 @@ import com.github.britooo.looca.api.group.processador.Processador;
 
 public class MonitoramentoCpu {
     Looca looca = new Looca();
-    Processador cpu = looca.getProcessador();
+    String cpuFabricante = looca.getProcessador().getFabricante();
+    String cpuId = looca.getProcessador().getId();
+    String cpuIdentificador = looca.getProcessador().getIdentificador();
+    String cpuNome = looca.getProcessador().getNome();
+    String cpuMicro = looca.getProcessador().getMicroarquitetura();
+    Long cpuFreq = looca.getProcessador().getFrequencia();
+    Integer cpuFisicas = looca.getProcessador().getNumeroCpusFisicas();
+    Integer cpuLogicas = looca.getProcessador().getNumeroCpusLogicas();
+    Double cpuUso = looca.getProcessador().getUso();
 
-    public Processador exibeCpu() {
-        return cpu;
+    public String exibeCpu() {
+        String cpuInfo = String.format("""
+        Fabricante: %s
+        Id: %s
+        Identificador: %s
+        Nome: %s
+        Micro-arquitetura: %s
+        Frequência: %s
+        Cpus Físicas: %d
+        Cpus Lógicas: %d
+        Uso da Cpu: %.2f""",
+                cpuFabricante, cpuId, cpuIdentificador, cpuNome, cpuMicro, cpuFreq, cpuFisicas, cpuLogicas, cpuUso);
+        return cpuInfo;
     }
 }
