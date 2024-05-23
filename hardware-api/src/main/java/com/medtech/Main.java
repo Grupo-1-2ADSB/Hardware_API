71d8075b33f20090a089fe19c53ec23d143471d1
kauanuness <kaua.souza@sptech.school>
2024-05-23 20:13:10 -0300

    feat(): deixando o código mais limpo e leve

--- hardware-api/src/main/java/com/medtech/Main.java
+++ hardware-api/src/main/java/com/medtech/Main.java
@@ -7,7 +7,6 @@ import com.medtech.dao.UsuarioDAO;
 import com.medtech.model.componente.armazenamento.Armazenamento;
 import com.medtech.model.componente.cpu.MonitoramentoCpu;
 import com.medtech.model.componente.memoria.MonitoramentoMemoria;
-import com.medtech.model.componente.rede.MonitoramentoRede;
 import com.medtech.model.usuario.Usuario;
 
 import java.sql.SQLException;
@@ -22,11 +21,24 @@ public class Main {
         Armazenamento disco01 = new Armazenamento();
         MonitoramentoMemoria memoria01 = new MonitoramentoMemoria();
         MonitoramentoCpu cpu01 = new MonitoramentoCpu();
-        MonitoramentoRede rede01 = new MonitoramentoRede();
 
         UsuarioDAO usuarioDAO = new UsuarioDAO();
         ComponenteDAO componenteDAO = new ComponenteDAO();
 
+        exibirBanner();
+
+        Usuario usuario = autenticarUsuario(scanner, usuarioDAO);
+        if (usuario != null && !usuario.getNomeUser().isEmpty()) {
+            exibirSistema(looca);
+            iniciarColetaDeDados(memoria01, cpu01, disco01, componenteDAO);
+        } else {
+            System.out.println("Usuário ou senha incorretos. Tente novamente mais tarde.");
+        }
+
+        scanner.close();
+    }
+
+    private static void exibirBanner() {
         System.out.println("""
                  __  __          _ _____         _    \s
                 |  \\/  | ___  __| |_   _|__  ___| |__ \s
@@ -34,57 +46,58 @@ public class Main {
                 | |  | |  __/ (_| | | |  __/ (__| | | |
                 |_|  |_|\\___|\\__,_| |_|\\___|\\___|_| |_|
                                                       \s""");
-
         System.out.println("=====================================");
+    }
 
+    private static Usuario autenticarUsuario(Scanner scanner, UsuarioDAO usuarioDAO) throws SQLException {
         System.out.print("Digite seu nome de usuário: ");
         String nomeUsuario = scanner.nextLine();
         System.out.print("Digite sua senha: ");
         String senhaUsuario = scanner.nextLine();
-
-        Usuario usuario = usuarioDAO.retornaUsuario(nomeUsuario, senhaUsuario);
-
-        if (usuario != null && !usuario.getNomeUser().isEmpty()) {
-            System.out.println("Login bem-sucedido!");
-            System.out.print("Pegando os dados do seu computador: ");
-
-            System.out.print("Carregando: [");
-            for (int i = 0; i <= 25; i++) {
-                System.out.print("█");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            System.out.println("] Concluído!");
-            System.out.println();
-            Sistema sistema = looca.getSistema();
-            System.out.println(sistema);
-            System.out.println("=====================================");
-
-            while (true) {
-                try {
-                    Thread.sleep(3000);
-                    System.out.println();
-                    System.out.println("Inserindo no Banco de Dados..");
-                    memoria01.exibeMemoria();
-                    System.out.println("Uso da CPU: %.2f GHz".formatted(cpu01.getUsoCpuGHz()));
-                    System.out.println("Armazenamento em uso: %.2f GB".formatted(disco01.getVolumes()));
-                    // Inserir os dados no banco de dados
-                    componenteDAO.inserirUsoMemoria(memoria01);
-                    componenteDAO.inserirUsoArmazenamento(disco01);
-                    componenteDAO.inserirUsoCpu(cpu01);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else {
-            System.out.println("Usuário ou senha incorretos. Tente novamente mais tarde");
-            scanner.close();
-        }
-
+        return usuarioDAO.retornaUsuario(nomeUsuario, senhaUsuario);
     }
+
+    private static void exibirSistema(Looca looca) {
+        System.out.print("Pegando os dados do seu computador: ");
+        for (int i = 0; i <= 25; i++) {
+            System.out.print("█");
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("] Concluído!");
+        Sistema sistema = looca.getSistema();
+        System.out.println(sistema);
+        System.out.println("=====================================");
+    }
+
+    private static void iniciarColetaDeDados(MonitoramentoMemoria memoria, MonitoramentoCpu cpu, Armazenamento armazenamento, ComponenteDAO componenteDAO) {
+        while (true) {
+            try {
+                Thread.sleep(3000);
+
+                double memoriaEmUso = memoria.getMemoriaEmUsoGB();
+                double usoCpu = cpu.getUsoCpuGHz();
+                double armazenamentoEmUso = armazenamento.getVolumes();
+
+                componenteDAO.inserirUsoMemoria(memoria);
+                componenteDAO.inserirUsoArmazenamento(armazenamento);
+                componenteDAO.inserirUsoCpu(cpu);
+
+                System.out.println("Dados atuais:");
+                System.out.println("Uso da Memória: " + String.format("%.2f", memoriaEmUso) + " GB");
+                System.out.println("Uso da CPU: " + String.format("%.2f", usoCpu) + " GHz");
+                System.out.println("Armazenamento em uso: " + String.format("%.2f", armazenamentoEmUso) + " GB");
+                System.out.println();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.out.println("Erro ao inserir dados no banco de dados: " + e.getMessage());
+            }
+        }
+    }
+
 }
--- hardware-api/src/main/java/com/medtech/dao/ComponenteDAO.java
+++ hardware-api/src/main/java/com/medtech/dao/ComponenteDAO.java
@@ -5,6 +5,7 @@ import com.medtech.model.componente.armazenamento.Armazenamento;
 import com.medtech.model.componente.cpu.MonitoramentoCpu;
 import com.medtech.model.componente.memoria.MonitoramentoMemoria;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,17 +13,17 @@ import java.util.Date;
 
 public class ComponenteDAO {
 
-    private ConexaoBanco conexaoBanco;
+    private final ConexaoBanco conexaoBanco;
 
     public ComponenteDAO() {
         this.conexaoBanco = new ConexaoBanco();
     }
 
-    MonitoramentoCpu cpu01 = new MonitoramentoCpu();
+    private MonitoramentoCpu cpu01 = new MonitoramentoCpu();
 
-    private boolean computadorExiste(String idComputador) throws SQLException {
+    private boolean computadorExiste(Connection conexao, String idComputador) throws SQLException {
         String sql = "SELECT COUNT(*) FROM Computador WHERE idComputador = ?";
-        try (PreparedStatement stmt = conexaoBanco.getConexao().prepareStatement(sql)) {
+        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
             stmt.setString(1, idComputador);
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
@@ -33,10 +34,10 @@ public class ComponenteDAO {
         return false;
     }
 
-    private void inserirComputadorSeNecessario(String idComputador) throws SQLException {
-        if (!computadorExiste(idComputador)) {
+    private void inserirComputadorSeNecessario(Connection conexao, String idComputador) throws SQLException {
+        if (!computadorExiste(conexao, idComputador)) {
             String sql = "INSERT INTO Computador (idComputador, nome, localizacao, statusPC, fkUnidadeHospitalar, fkSO) VALUES (?, ?, ?, ?, ?, ?)";
-            try (PreparedStatement stmt = conexaoBanco.getConexao().prepareStatement(sql)) {
+            try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
                 stmt.setString(1, idComputador);
                 stmt.setString(2, "NomePadrao"); // Substitua com valores apropriados
                 stmt.setString(3, "LocalizacaoPadrao"); // Substitua com valores apropriados
@@ -44,44 +45,39 @@ public class ComponenteDAO {
                 stmt.setInt(5, 1); // Substitua com a fkUnidadeHospitalar apropriada
                 stmt.setInt(6, 1); // Substitua com a fkSO apropriada
                 stmt.executeUpdate();
-
             }
         }
     }
 
-    public void inserirUsoMemoria(MonitoramentoMemoria memoria) throws SQLException {
-        inserirComputadorSeNecessario(cpu01.getIdCPU()); // Verifica e insere o computador se necessário
+    private void inserirRegistro(Connection conexao, double valor, String idComputador, int fkHardware) throws SQLException {
         String sql = "INSERT INTO Registro (valor, dataHora, fkComputador, fkHardware) VALUES (?, ?, ?, ?)";
-        try (PreparedStatement stmt = conexaoBanco.getConexao().prepareStatement(sql)) {
-            stmt.setDouble(1, memoria.getMemoriaEmUsoGB());
+        try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+            stmt.setDouble(1, valor);
             stmt.setTimestamp(2, new java.sql.Timestamp(new Date().getTime()));
-            stmt.setString(3, cpu01.getIdCPU()); // ID do computador = idCPU
-            stmt.setInt(4, 1); // Supondo que 1 seja o ID correspondente ao hardware de memória RAM
+            stmt.setString(3, idComputador);
+            stmt.setInt(4, fkHardware);
             stmt.executeUpdate();
+        }
+    }
+
+    public void inserirUsoMemoria(MonitoramentoMemoria memoria) throws SQLException {
+        try (Connection conexao = conexaoBanco.getConexao()) {
+            inserirComputadorSeNecessario(conexao, cpu01.getIdCPU());
+            inserirRegistro(conexao, memoria.getMemoriaEmUsoGB(), cpu01.getIdCPU(), 1); // 1 é o ID correspondente ao hardware de memória RAM
         }
     }
 
     public void inserirUsoArmazenamento(Armazenamento armazenamento) throws SQLException {
-        inserirComputadorSeNecessario(cpu01.getIdCPU()); // Verifica e insere o computador se necessário
-        String sql = "INSERT INTO Registro (valor, dataHora, fkComputador, fkHardware) VALUES (?, ?, ?, ?)";
-        try (PreparedStatement stmt = conexaoBanco.getConexao().prepareStatement(sql)) {
-            stmt.setDouble(1, armazenamento.getVolumes());
-            stmt.setTimestamp(2, new java.sql.Timestamp(new Date().getTime()));
-            stmt.setString(3, cpu01.getIdCPU()); // ID do computador = idCPU
-            stmt.setInt(4, 2); // Supondo que 2 seja o ID correspondente ao hardware de armazenamento
-            stmt.executeUpdate();
+        try (Connection conexao = conexaoBanco.getConexao()) {
+            inserirComputadorSeNecessario(conexao, cpu01.getIdCPU());
+            inserirRegistro(conexao, armazenamento.getVolumes(), cpu01.getIdCPU(), 2); // 2 é o ID correspondente ao hardware de armazenamento
         }
     }
 
     public void inserirUsoCpu(MonitoramentoCpu cpu) throws SQLException {
-        inserirComputadorSeNecessario(cpu01.getIdCPU()); // Verifica e insere o computador se necessário
-        String sql = "INSERT INTO Registro (valor, dataHora, fkComputador, fkHardware) VALUES (?, ?, ?, ?)";
-        try (PreparedStatement stmt = conexaoBanco.getConexao().prepareStatement(sql)) {
-            stmt.setDouble(1, cpu.getUsoCpuGHz());
-            stmt.setTimestamp(2, new java.sql.Timestamp(new Date().getTime()));
-            stmt.setString(3, cpu01.getIdCPU()); // ID do computador = idCPU
-            stmt.setInt(4, 3); // Supondo que 3 seja o ID correspondente ao hardware de CPU
-            stmt.executeUpdate();
+        try (Connection conexao = conexaoBanco.getConexao()) {
+            inserirComputadorSeNecessario(conexao, cpu01.getIdCPU());
+            inserirRegistro(conexao, cpu.getUsoCpuGHz(), cpu01.getIdCPU(), 3); // 3 é o ID correspondente ao hardware de CPU
         }
     }
 }
